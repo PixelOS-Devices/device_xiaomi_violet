ca93f0174a830c62dbd0c3a1b5a0466273d78b7c
Karan Parashar <whyredfire@gmail.com>
2021-12-27 17:09:11 +0530

    violet: Nuke duplicate definition - previous definition here https://github.com/PixelOS-Pixelish/vendor_aosp/blob/twelve/config/common_full_phone.mk#L3

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -10,7 +10,6 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-IS_PHONE := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_violet
