c33a9471fa8c6feb3df806ada410134a33768324
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-13 16:19:06 +0530

    violet: Go away Megvii

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+PRODUCT_PACKAGES += \
+	libMegviiFacepp-0.5.2 \
+	libmegface
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
