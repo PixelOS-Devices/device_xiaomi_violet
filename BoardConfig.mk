f6c0d32f1d04f3555167d06f5c64323ee38998f2
chethazz <chethaskp10@gmail.com>
2022-11-24 21:59:54 +0530

    violet: Drop proton clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,9 +64,6 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
 TARGET_KERNEL_CONFIG := violet_defconfig
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 BOARD_RAMDISK_OFFSET := 0x01000000
 
 # Platform
