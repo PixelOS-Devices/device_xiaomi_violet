4f69680209703c0c3348fc09f11747bab1ed0183
chethazz <chethaskp10@gmail.com>
2023-01-09 20:14:02 +0530

    violet: Switch to common_full_phone

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit ANX Camera
 $(call inherit-product, vendor/ANXCamera/config.mk)
